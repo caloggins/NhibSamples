6e90d8c09e01ad195f0a65610cb07dab1d4900fd
cloggins <christopher.loggins@gmail.com>
2012-11-15 09:05:04 -0600

    Removed some excells SQL.

--- Prabu/DataBaseScript.sql
+++ Prabu/DataBaseScript.sql
@@ -1,45 +1,37 @@
-﻿begin try
-	begin tran;
-	create table [Users] (
-		Id bigint primary key identity
-		,Name varchar(max) not null
+﻿create table [Users] (
+	Id bigint primary key identity
+	,Name varchar(max) not null
+);
+
+create table UserDetail (
+	UserId bigint primary key
+	,DriversLicense varchar(max)
+	,IsDonor bit not null default 0
+);
+
+create table Customer (
+	Id bigint primary key identity
+	,Name varchar(max) not null
+	,DateOfBirth date not null default getdate()
+	);
+		
+create table CustomerAccount (
+	CustomerId bigint
+	,AccountId bigint
+	,[Ownership] varchar(max)
+	primary key (CustomerId, AccountId)
 	);
 
-	create table UserDetail (
-		UserId bigint primary key
-		,DriversLicense varchar(max)
-		,IsDonor bit not null default 0
-	);
+create table Account (
+	Id bigint primary key identity
+	,AccountNumber varchar(max) not null
+)
 
-	create table Customer (
-		Id bigint primary key identity
-		,Name varchar(max) not null
-		,DateOfBirth date not null default getdate()
-		);
+alter table UserDetail with check
+	add constraint FK_User_Id foreign key (UserId) references [Users](Id);
 		
-	create table CustomerAccount (
-		CustomerId bigint
-		,AccountId bigint
-		,[Ownership] varchar(max)
-		primary key (CustomerId, AccountId)
-		);
-
-	create table Account (
-		Id bigint primary key identity
-		,AccountNumber varchar(max) not null
-	)
-
-	alter table UserDetail with check
-		add constraint FK_User_Id foreign key (UserId) references [Users](Id);
+alter table CustomerAccount with check
+	add constraint FK_Customer_Id foreign key (CustomerId) references Customer(Id);
 		
-	alter table CustomerAccount with check
-		add constraint FK_Customer_Id foreign key (CustomerId) references Customer(Id);
-		
-	alter table CustomerAccount with check
-		add constraint FK_Account_Id foreign key (AccountId) references Account(Id);
-	commit tran;
-end try
-begin catch
-	rollback tran;
-end catch;
-
+alter table CustomerAccount with check
+	add constraint FK_Account_Id foreign key (AccountId) references Account(Id);
