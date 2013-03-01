fef444eeae328b2f803d185080b2a8fd935fdd3e
Christopher Loggins <christopher.loggins@gmail.com>
2013-03-01 09:34:18 -0600

    Basic Program
    Added program, wich EventLog example.

---
+++ TopShelfWcfExample/TopShelfWcfExample.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TopShelfWcfExample.ConsoleApp
+{
+    using System;
+    using System.Diagnostics;
+
+    public class Program
+    {
+        static void Main()
+        {
+            try
+            {
+            }
+            catch (Exception exception)
+            {
+                var assemblyName = typeof(Program).AssemblyQualifiedName;
+
+                if (!EventLog.SourceExists(assemblyName))
+                    EventLog.CreateEventSource(assemblyName, "Application");
+
+                var log = new EventLog { Source = assemblyName };
+                log.WriteEntry(string.Format("{0}", exception), EventLogEntryType.Error);
+            }
+        }
+    }
+}
